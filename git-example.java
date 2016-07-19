71e6a7fa46e456586a8790867ed493af0ba94ca6
karlesbelanger <karlesbelanger@gmail.com>
2016-07-19 17:08:11 -0400

    Multiply  function

--- git-example.java
+++ git-example.java
@@ -2,4 +2,10 @@ public class ExampleGit{
 	public static void main(){
 		system.out.println("Hello world");
 	}
+	public static int sum(int n1, int n2){
+		return n1+n2;
+	}
+	public static int multiply(int n1, int n2){
+		return n1*n2;
+	}
 }
